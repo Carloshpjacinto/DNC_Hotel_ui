1280a6644f0ffbae5f04078bd9e34b53aa133508
Carloshpjacinto <carlo.henrique37@gmail.com>
2025-02-18 16:53:16 -0300

    feature/components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import Image from "next/image";
+import Button from "@/components/Button";
+import Link from "@/components/Link";
+import Pagination from "@/components/Pagination";
+import Alert from "@/components/Alert";
 
 export default function Home() {
   return (
-    <div className="flex justify-center mt-20">
-      <h3>Text</h3>
-      <h3>Text2</h3>
-    </div>
+    <section>
+      pagina principal
+      <Button>clique em mim</Button>
+      <Button appearance="secondary">clique em mim</Button>
+      <Link href="/teste">Ir para teste</Link>
+      <Pagination currentPage={98} destination="/" totalPages={100} />
+      <Alert type="success">Esse é um feedback de sucesso</Alert>
+      <Alert type="danger">Falha na operação, tente novamente</Alert>
+    </section>
   );
 }
