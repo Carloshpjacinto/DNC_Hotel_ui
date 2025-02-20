f17723cfeac213ec9101248812dfbd815e5af0ef
Carloshpjacinto <carlo.henrique37@gmail.com>
2025-02-20 15:31:22 -0300

    feature/finalizando-components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Button from "@/components/Button";
+import ImageField from "@/components/Form/ImageField";
+import TextField from "@/components/Form/TextField";
 import Link from "@/components/Link";
 import Pagination from "@/components/Pagination";
 import Alert from "@/components/Alert";
@@ -10,6 +12,8 @@ export default function Home() {
       <Button>clique em mim</Button>
       <Button appearance="secondary">clique em mim</Button>
       <Link href="/teste">Ir para teste</Link>
+      <TextField label="Nome completo" id="full_name" name="full_name" />
+      <ImageField label="Selecione a foto" id="profile_picture" />
       <Pagination currentPage={98} destination="/" totalPages={100} />
       <Alert type="success">Esse é um feedback de sucesso</Alert>
       <Alert type="danger">Falha na operação, tente novamente</Alert>
